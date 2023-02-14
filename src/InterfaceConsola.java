20b00e79779f2a6e631a8f16aa228e018d89079f
angel <angel.gomez.prol@udc.es>
2023-02-14 22:43:28 +0100

    Cositas

--- src/InterfaceConsola.java
+++ src/InterfaceConsola.java
@@ -7,22 +7,25 @@ public class InterfaceConsola {
         System.out.println("1| * | * | * |");
         System.out.println("2| * | * | * |");
         System.out.println("3| * | * | * |");
+        System.out.println("La maquina va a usar 'O' como simbolo y tú vas a usar 'X' ");
         System.out.println("Tu selecciona que posicion quieras con ordenes como pEj.: B2");
         System.out.println("BUENA SUERTE");
+
     }
     public void gamecontroller(){
         printmapa();
     }
 
     public void printmapa(){
-        char[] mapa = new char[9];
+        char[] mapa = new char[10];
         mapa[1]='a';
         mapa[2]='b';
         mapa[3]='c';
         System.out.println(" | A | B | C |");
         System.out.printf("1| %c | %c | %c |\n",mapa[1],mapa[2],mapa[3]);
-        System.out.println("2| * | * | * |");
-        System.out.println("3| * | * | * |");
+        System.out.printf("1| %c | %c | %c |\n",mapa[4],mapa[5],mapa[6]);
+        System.out.printf("1| %c | %c | %c |\n",mapa[7],mapa[8],mapa[9]);
+
     }
 
 
--- src/motor3R/TresEnRaya.java
+++ src/motor3R/TresEnRaya.java
@@ -1,7 +1,27 @@
 package motor3R;
 
 public class TresEnRaya {
+    char[] tablero;
+   public boolean hayganador(char[] tablero){
+       if (tablero[1]==tablero[2] && tablero[1]==tablero[3]){
+           return true;
+       } else if (tablero[4]==tablero[5] && tablero[4]==tablero[6]) {
+           return true;
+       } else if (tablero[7]==tablero[8] && tablero[7]==tablero[9]) {
+           return true;
+       }
 
-   // public void check
+       if (tablero[1]==tablero[4] && tablero[1]==tablero[7]){
+           return true;
+       } else if (tablero[2]==tablero[5] && tablero[2]==tablero[8]) {
+           return true;
+       } else if (tablero[3]==tablero[6] && tablero[3]==tablero[9]) {
+           return true;
+       }
+
+
+       return false;
+   }
+
 
 }
