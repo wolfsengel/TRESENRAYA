c9599ddd6b3e1f5c87e73e7bb8c14c0553ad59ea
a22angelgp <a22angelgp@iessanclemente.net>
2023-02-15 10:31:04 +0100

    logica, falta ganador detect

--- src/InterfaceConsola.java
+++ src/InterfaceConsola.java
@@ -1,33 +1,51 @@
 import motor3R.TresEnRaya;
+import java.util.Scanner;
 public class InterfaceConsola {
     public void presentacion(){
         System.out.println("Bienvenidos al tres en RAYA");
         System.out.println("ESTE ES EL CAMPO DE JUEGO:");
-        System.out.println(" | A | B | C |");
+        System.out.println(" | 1 | 2 | 3 |");
         System.out.println("1| * | * | * |");
         System.out.println("2| * | * | * |");
         System.out.println("3| * | * | * |");
         System.out.println("La maquina va a usar 'O' como simbolo y tú vas a usar 'X' ");
-        System.out.println("Tu selecciona que posicion quieras con ordenes como pEj.: B2");
+        System.out.println("Tu selecciona que posicion quieras con ordenes como pEj.: 32\n" +
+                "Empezando por el eje X primero :)");
         System.out.println("BUENA SUERTE");
 
     }
     public void gamecontroller(){
-        printmapa();
+        TresEnRaya jueguito = new TresEnRaya();
+        Scanner sc = new Scanner(System.in);
+        String[] posicion;
+        printmapa(jueguito.tablero);
+        while(true){
+            System.out.println("Dime la posicion deseada: ");
+            posicion=sc.nextLine().split("");
+            jueguito.insercion(posicion);
+            jueguito.insercionM(jueguito.turnomaquina());
+            printmapa(jueguito.tablero);
+            if (jueguito.getWinner() == 'X') {
+                System.out.println("Congratulations! You win!");
+                break;
+            } else if (jueguito.getWinner() == 'O') {
+                System.out.println("Sorry, the computer wins.");
+                break;
+            } else{
+                System.out.println("It's a tie!");
+                break;
+            }
+        }
+
     }
 
-    public void printmapa(){
-        char[] mapa = new char[10];
-        mapa[1]='a';
-        mapa[2]='b';
-        mapa[3]='c';
+    public void printmapa(char[][] mapa){
         System.out.println(" | A | B | C |");
-        System.out.printf("1| %c | %c | %c |\n",mapa[1],mapa[2],mapa[3]);
-        System.out.printf("1| %c | %c | %c |\n",mapa[4],mapa[5],mapa[6]);
-        System.out.printf("1| %c | %c | %c |\n",mapa[7],mapa[8],mapa[9]);
+        System.out.printf("1| %c | %c | %c |\n",mapa[1][1],mapa[1][2],mapa[1][3]);
+        System.out.printf("1| %c | %c | %c |\n",mapa[2][1],mapa[2][2],mapa[2][3]);
+        System.out.printf("1| %c | %c | %c |\n",mapa[3][1],mapa[3][2],mapa[3][3]);
 
     }
-
 
 
     public static void main(String[] args) {
--- src/motor3R/TresEnRaya.java
+++ src/motor3R/TresEnRaya.java
@@ -1,27 +1,96 @@
 package motor3R;
 
+import java.util.Random;
+
 public class TresEnRaya {
-    char[] tablero;
-   public boolean hayganador(char[] tablero){
-       if (tablero[1]==tablero[2] && tablero[1]==tablero[3]){
-           return true;
-       } else if (tablero[4]==tablero[5] && tablero[4]==tablero[6]) {
-           return true;
-       } else if (tablero[7]==tablero[8] && tablero[7]==tablero[9]) {
-           return true;
-       }
+    public char[][] tablero = { {' ',' ',' ',' '},{' ',' ',' ',' '},{' ',' ',' ',' '},{' ',' ',' ',' '}};
+    public String aux;
 
-       if (tablero[1]==tablero[4] && tablero[1]==tablero[7]){
-           return true;
-       } else if (tablero[2]==tablero[5] && tablero[2]==tablero[8]) {
-           return true;
-       } else if (tablero[3]==tablero[6] && tablero[3]==tablero[9]) {
-           return true;
-       }
+   public void insercion(String[] cositas){
 
+       tablero[Integer.parseInt(cositas[0])][Integer.parseInt(cositas[1])] = 'X';
 
-       return false;
    }
 
+    public int[] turnomaquina() {
+        // Check if the computer can win on this move
+        for (int row = 1; row < 4; row++) {
+            for (int col = 1; col < 4; col++) {
+                if (tablero[row][col] == ' ') {
+                    tablero[row][col] = 'O';
+                    if (getWinner() == 'O') {
+                        return new int[] { row, col };
+                    }
+                    tablero[row][col] = ' ';
+                }
+            }
+        }
 
+        // Check if the player can win on the next move and block it
+        for (int row = 1; row < 4; row++) {
+            for (int col = 1; col < 4; col++) {
+                if (tablero[row][col] == ' ') {
+                    tablero[row][col] = 'X';
+                    if (getWinner() == 'X') {
+                        return new int[] { row, col };
+                    }
+                    tablero[row][col] = ' ';
+                }
+            }
+        }
+
+        // Choose a random move
+        Random random = new Random();
+        int row, col;
+        do {
+            row = random.nextInt(1,4);
+            col = random.nextInt(1,4);
+        } while (tablero[row][col] != ' ');
+
+        return new int[] { row, col };
+    }
+
+
+    public char getWinner() {
+        // Check rows
+        for (int row = 0; row < 3; row++) {
+            if (tablero[row][0] == tablero[row][1] && tablero[row][1] == tablero[row][2] && tablero[row][0] != ' ') {
+                return tablero[row][0];
+            }
+        }
+
+        // Check columns
+        for (int col = 0; col < 3; col++) {
+            if (tablero[0][col] == tablero[1][col] && tablero[1][col] == tablero[2][col] && tablero[0][col] != ' ') {
+                return tablero[0][col];
+            }
+        }
+
+        // Check diagonals
+        if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2] && tablero[0][0] != ' ') {
+            return tablero[0][0];
+        }
+
+        if (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0] && tablero[0][2] != ' ') {
+            return tablero[0][2];
+        }
+
+        // Check if the game is a tie
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (tablero[row][col] == ' ') {
+                    // There are still empty positions on the tablero, so the game is not over yet
+                    return ' ';
+                }
+            }
+        }
+
+        // If no winner and no empty positions left, then it's a tie
+        return 'T';
+    }
+
+
+    public void insercionM(int[] turnomaquina) {
+        tablero[turnomaquina[0]][turnomaquina[1]] = 'O';
+    }
 }
